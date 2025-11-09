46ce7c3bb20e3481791b012fb0400b1525c61d42
Sarah Sharp <sarah.sharpish@gmail.com>
2025-11-09 13:43:19 -0600

    Added Scratchwork file

--- Batching_and_Extracting.py
+++ Batching_and_Extracting.py
@@ -10,25 +10,12 @@ def extract_ids():
         result.append(track.removeprefix("https://open.spotify.com/track/").rstrip())
     return result
 
-def get_batches():
-    pass
+def get_batches(arr):
+    length = len(arr)
+    for i in range(0,length-1,6):
+        yield arr[i, i+6]
+  
 
-
-
-
-    
-def grouper(iterable, num, incomplete = 'fill', fillvalue=None):
-    '''Collect data into non-overlapping fixed-length batches'''
-    iterators = [iter(iterable)] * num
-    match incomplete:
-        case 'fill':
-            return zip_longest(*iterators, fillvalue=fillvalue)
-        case 'strict':
-            return zip(*iterators, strict=True)
-        case 'ignore':
-            return zip(*iterators)
-        case _:
-            raise ValueError('Expected fill, strict, or ignore')
     
 
         
@@ -36,4 +23,5 @@ def grouper(iterable, num, incomplete = 'fill', fillvalue=None):
 if __name__ == "__main__":
     # print(extract_ids())
     # print(get_batches())
-    print(grouper(iterable=[1,2,3,4,5,5,6,7,8,8,9,9,9,6,5,4,3],num=5, incomplete='fill', fillvalue=None))
+    # print(grouper(iterable=[1,2,3,4,5,5,6,7,8,8,9,9,9,6,5,4,3],num=5, incomplete='fill', fillvalue=None))
+    print(get_batches([1,2,3,4,5,5,6,7,8,8,9]))
