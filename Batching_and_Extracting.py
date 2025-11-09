8be7568e21773895972a9db75d1405afa1905740
Sarah Sharp <sarah.sharpish@gmail.com>
2025-11-09 16:55:13 -0600

    Fixed function calls and flow

--- Batching_and_Extracting.py
+++ Batching_and_Extracting.py
@@ -4,24 +4,33 @@ from itertools import zip_longest
 with open("Full_Links.txt", "r") as file:
     full_links = file.readlines()
 
-def extract_ids():
-    result = []
-    for track in full_links:
-        result.append(track.removeprefix("https://open.spotify.com/track/").rstrip())
-    return result
+def clean_links(full_links):
+    cleaned = []
+    for link in full_links:
+        cleaned.append(link.rstrip())
+    return cleaned
 
-def get_batches(arr):
-    length = len(arr)
-    for i in range(0,length-1,6):
-        yield arr[i, i+6]
-  
+def extract_ids(batch):
+    parsed_ids = []
+    for track in batch:
+        parsed_ids.append(track.removeprefix("https://open.spotify.com/track/").rstrip())
+    return parsed_ids
 
-    
+def get_batches(parsed_ids):
+    length = len(parsed_ids)
+    for i in range(0,length-1,10):
+        yield parsed_ids[i:i+10]
 
+def consume_batches():
+    raw_batch = get_batches(full_links)
+    for b in raw_batch:
+        batch = extract_ids(raw_batch)
+        return batch
         
 
 if __name__ == "__main__":
     # print(extract_ids())
     # print(get_batches())
     # print(grouper(iterable=[1,2,3,4,5,5,6,7,8,8,9,9,9,6,5,4,3],num=5, incomplete='fill', fillvalue=None))
-    print(get_batches([1,2,3,4,5,5,6,7,8,8,9]))
+    # print(get_batches([1,2,3,4,5,5,6,7,8,8,9]))
+    print(consume_batches())
