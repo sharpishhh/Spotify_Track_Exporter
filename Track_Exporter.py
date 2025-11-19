546f055e4386128b4340f2f5250d9ed9c61cb1df
Sarah Sharp <sarah.sharpish@gmail.com>
2025-11-18 20:47:27 -0600

    Removed count variable; Removed start index and created current row which is increased incrementally with each batch length

--- Track_Exporter.py
+++ Track_Exporter.py
@@ -9,6 +9,8 @@ load_dotenv()
 print("starting track exporter.py")
 
 
+
+
 def authorization(client_id, client_secret, redirect_uri, scope):
     # Use keyword arguments so SpotifyOAuth gets the right values
     auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
@@ -16,18 +18,19 @@ def authorization(client_id, client_secret, redirect_uri, scope):
     return sp
 
 # Get track information from Spotify Web API
-def retrieve_track_data(sp, batch, start_index):
+def retrieve_track_data(sp, batch, current_row):
     track_info = {}
     trk_info_list = []
     track_list = sp.tracks(batch)
     for i, track in enumerate(track_list['tracks']):
-        row_num = start_index + i
+        row_num = current_row + i
         artists = []
         for artist in track['artists']:
             artists.append(artist['name'])
         song_artists = ','.join(artists)
         track_info = {'Track ID': row_num, 'Song': track['name'], 'Artist(s)': song_artists}
         trk_info_list.append(track_info)
+    
     return trk_info_list
 
 # Create time delay between retrieving batches
@@ -87,7 +90,7 @@ def run_exporter(client_id, client_secret, redirect_uri, scope):
     if os.path.exists("track_list.csv"):
         print("Removing existing track_list.csv")
         os.remove("track_list.csv")
-    start_index = 1
+    current_row = 0
     full_links = read_links("Full_Links.txt")
     cleaned = clean_links(full_links)
     parsed_ids = extract_ids(cleaned)
@@ -96,8 +99,9 @@ def run_exporter(client_id, client_secret, redirect_uri, scope):
     batch_count = 0
     for batch in batches:
         batch_count += 1
-        track_info = retrieve_track_data(sp, batch, start_index)
+        track_info = retrieve_track_data(sp, batch, current_row)
         consume_batches(track_info)
+        current_row += len(batch)
         time_delay(.25)
     print(f"Created {batch_count} batches total")
     
